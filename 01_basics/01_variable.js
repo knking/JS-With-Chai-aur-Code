b71ac5bc9f7478a3871da8f4e65a6fe4b92abaf6
KRISHNANAND ROY <39957660+knking@users.noreply.github.com>
2023-07-24 11:58:25 +0000

    javaScript basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+console.log("Hello Krishna")
+
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountCity = "noida",
+
+accountstate="UP"
+console.log(accountstate)
+
+console.table([accountId,accountEmail,accountCity,accountstate])
